696f7fcada560f095bf2b8467fb2ea0662a269d4
Sandro Seivane <seivanesandro@gmail.com>
2024-04-15 14:17:23 +0100

    add navbar bootstrap to project and add some styles

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import NavbarCanvas from "./components/navbar/Navbar";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavbarCanvas from './components/navbar/Navbar';
 //import Footer from "./components/footer/Footer";
 
 function App() {
-  return (
-    <div className="App">
-      <NavbarCanvas />
-      Tour & Travel
-      {/* <footer>
+    return (
+        <div className="App">
+            <NavbarCanvas />
+            Tour & Travel
+            {/* <footer>
         <Footer />
       </footer> */}
-    </div>
-  );
+        </div>
+    );
 }
 
 export default App;
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,48 +1,55 @@
-import React from "react";
+import React from 'react';
 //import PropTypes from 'prop-types'
-import styled from "styled-components";
+import styled from 'styled-components';
 
 const ContainerFooter = styled.div`
-  padding: 3rem 0 1rem 0;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  gap: 1rem;
-  background: #333;
-  color: #aba7a7;
-  text-transform: lowercase;
-  letter-spacing: 2px;
-  font-size: 0.9rem !important;
+    padding: 3rem 0 1rem 0;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 1rem;
+    background: #333;
+    color: #aba7a7;
+    text-transform: lowercase;
+    letter-spacing: 2px;
+    font-size: 0.9rem !important;
 `;
 const FooterLabel = styled.label``;
 const FooterName = styled.span``;
 
-const Footer = (props) => {
-  return (
-    <>
-      <ContainerFooter className="container-footer">
-        <FooterLabel htmlFor="footer" className="footer-label">
-          <FooterName name="footer" className="code">
-            sandroseivane@copyrights - 2024
-          </FooterName>
-        </FooterLabel>
-        <p>
-          <a href="http://jigsaw.w3.org/css-validator/check/referer">
-            <img
-              style={{
-                border: "0",
-                width: "88px",
-                height: "31px",
-              }}
-              src="http://jigsaw.w3.org/css-validator/images/vcss-blue"
-              alt="Valid CSS!"
-            />
-          </a>
-        </p>
-      </ContainerFooter>
-    </>
-  );
+const Footer = props => {
+    return (
+        <>
+            <ContainerFooter className="container-footer">
+                <FooterLabel
+                    htmlFor="footer"
+                    className="footer-label"
+                >
+                    <FooterName
+                        name="footer"
+                        className="code"
+                    >
+                        sandroseivane@copyrights -
+                        2024
+                    </FooterName>
+                </FooterLabel>
+                <p>
+                    <a href="http://jigsaw.w3.org/css-validator/check/referer">
+                        <img
+                            style={{
+                                border: '0',
+                                width: '88px',
+                                height: '31px'
+                            }}
+                            src="http://jigsaw.w3.org/css-validator/images/vcss-blue"
+                            alt="Valid CSS!"
+                        />
+                    </a>
+                </p>
+            </ContainerFooter>
+        </>
+    );
 };
 
 Footer.propTypes = {};
